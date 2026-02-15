91ccbcf20302ae3cd6f539a56fa3d5902d03b958
Alex S <olstakh@microsoft.com>
2026-02-15 12:37:49 -0800

    add renameme project

---
+++ src/RenameMe/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace RenameMe;
+
+/// <summary>
+/// This is a placeholder class in the RenameMe project. It serves as an example of a class that would be part of the assembly being analyzed by the RenameMeAnalyzer. The presence of this class allows the analyzer to check the assembly name and report diagnostics if it is still named "RenameMe". In a real project, this class would contain actual implementation code relevant to the project's functionality.
+/// </summary>
+public class Class1
+{
+
+}
